020b49f071198da091555779be9b75c3029a7802
dd-thuan <67511121+dd-thuan@users.noreply.github.com>
2022-06-29 15:14:55 +0700

    setup and config folders

--- backend/app.js
+++ backend/app.js
@@ -3,14 +3,24 @@ const app = express();
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const path = require("path");
+const dotenv = require("dotenv");
+const { error } = require("console");
 
 
-app.use(bodyParser.json({limit:"30mb", extened:"true"}))
+app.use(bodyParser.json({ limit: "30mb", extened: "true" }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: "true" }));
 
 app.use(express.static(path.join(__dirname, "../frontend/build")));
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
-})
+});
 
+dotenv.config({path:"backend/config/config.env"});
+
+
+mongoose.connect(process.env.DB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then((data) => console.log(`MongoDb connected with server ${data.connection.host}`)).catch((error) => console.log(error));
 
 module.exports = app;
